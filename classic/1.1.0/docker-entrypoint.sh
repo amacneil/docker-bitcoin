3c32419708141c5c505e643dfc67c0f996ce1685
roshii <roshii@debian-dev.kasta>
2017-07-14 18:47:50 +0200

    Allow Docker's default subnet for API call

--- classic/1.1.0/docker-entrypoint.sh
+++ classic/1.1.0/docker-entrypoint.sh
@@ -9,6 +9,8 @@ if [ "$1" = 'bitcoin-cli' -o "$1" = 'bitcoin-tx' -o "$1" = 'bitcoind' -o "$1" = 
 		printtoconsole=1
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+		# Allow Docker's default subnet for API call
+		rpcallowip=172.17.0.0/16
 		EOF
 	fi
 
